9f19354240d85ea56834d3ccab0895ce60e4ad9a
Miiss Neko <guillaume.chateauroux@gmail.com>
2021-05-11 16:50:31 +0200

    Added: port to env file example Fix: missing title in main page render

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { address: process.env.MINING_ADDRESS });
+  res.render('index', { title: 'Ethermine Reporting', address: process.env.MINING_ADDRESS });
 });
 
 module.exports = router;
